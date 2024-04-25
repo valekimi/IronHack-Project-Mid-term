f565ba92c98a5e0c7a07e86eb7d9e816875562aa
Valentina Chimento <fetecchia@Valentinas-MacBook-Pro.local>
2024-04-25 13:45:54 +0200

    update day 4

--- javascript/2_Project.js
+++ javascript/2_Project.js
@@ -1,4 +1,3 @@
-
 //==========================================
 // FETCH Data MAIN PROJECT
 
@@ -18,7 +17,8 @@ async function getData() {
 
     document.getElementById("projectTitle").textContent = name;
     document.getElementById("projectTopic").textContent = description;
-    document.getElementById("projectDate").textContent =  "Completed on " + completed_on;
+    document.getElementById("projectDate").textContent =
+      "Completed on " + completed_on;
     document.getElementById("projectDescription").innerHTML = content;
 
     const projectCover = document.getElementById("projectCover");
@@ -27,35 +27,37 @@ async function getData() {
     img.style.width = "100%";
     img.style.height = "400px";
 
-    document.getElementById("filterBlur").innerHTML = `<img src="${image}" alt="">`;
+    document.getElementById(
+      "filterBlur"
+    ).innerHTML = `<img src="${image}" alt="">`;
 
     //I add the class to the children of #filterBlur
-    document.querySelector("#filterBlur img").classList.add("filterBlur")
+    document.querySelector("#filterBlur img").classList.add("filterBlur");
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
 getData();
 
-
 //==========================================
 // FETCH Data OTHER PROJECTS
 
-const projectCards = document.querySelectorAll('.project-card');
+const projectCards = document.querySelectorAll(".project-card");
 
-fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
-
-  .then(response => response.json())
-  .then(data => {
-
+fetch(
+  "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
+)
+  .then((response) => response.json())
+  .then((data) => {
     const reverseData = data.slice().reverse();
 
     for (let i = 1; i < Math.min(reverseData.length, 4); i++) {
       const projectData = reverseData[i];
-      projectCards[i-1].querySelector('.cover').src = projectData.image;
-      projectCards[i-1].querySelector('.title').textContent = projectData.name;
-      projectCards[i-1].querySelector('.description').textContent = projectData.description;
+      projectCards[i - 1].querySelector(".cover").src = projectData.image;
+      projectCards[i - 1].querySelector(".title").textContent =
+        projectData.name;
+      projectCards[i - 1].querySelector(".description").textContent =
+        projectData.description;
     }
   })
-  .catch(error => console.error('Error fetching data:', error));
-  
+  .catch((error) => console.error("Error fetching data:", error));
--- javascript/3_Contact.js
+++ javascript/3_Contact.js
@@ -1,54 +1,67 @@
-
 //===============================
 /* FORM VALIDATIONS */
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    const inputs = document.querySelectorAll('input[required], textarea[required]');
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    const inputs = document.querySelectorAll(
+      "input[required], textarea[required]"
+    );
     let isValid = true;
-    inputs.forEach(function(input) {
-        if (!input.value.trim()) {
-            isValid = false;
-            showError(input);
-        }
+    inputs.forEach(function (input) {
+      if (!input.value.trim()) {
+        isValid = false;
+        showError(input);
+      }
     });
     if (!isValid) {
-        event.preventDefault();
+      event.preventDefault();
     }
-});
+  });
 
 function showError(element) {
-    console.log(element);
-    element.style.border = '2px solid #ff0000';
-    const errorMessageElement = element.nextElementSibling;
+  console.log(element);
+  element.style.border = "2px solid #ff0000";
+  const errorMessageElement = element.nextElementSibling;
 
-    if (errorMessageElement && errorMessageElement.classList.contains('error-message')) {
-        errorMessageElement.textContent = 'This field is required.';
-    }
+  if (
+    errorMessageElement &&
+    errorMessageElement.classList.contains("error-message")
+  ) {
+    errorMessageElement.textContent = "This field is required.";
+  }
 }
 
-document.querySelectorAll('input[required], textarea[required]').forEach(function(input) {
-    input.addEventListener('blur', function() {
-        if (!this.value.trim()) {
-            showError(this);
+document
+  .querySelectorAll("input[required], textarea[required]")
+  .forEach(function (input) {
+    input.addEventListener("blur", function () {
+      if (!this.value.trim()) {
+        showError(this);
+      } else {
+        this.style.borderColor = "";
+        const errorMessageElement = this.nextElementSibling;
 
-        } else {
-            this.style.borderColor = '';
-            const errorMessageElement = this.nextElementSibling;
-
-            if (errorMessageElement && errorMessageElement.classList.contains('error-message')) {
-                errorMessageElement.textContent = '';
-            }
+        if (
+          errorMessageElement &&
+          errorMessageElement.classList.contains("error-message")
+        ) {
+          errorMessageElement.textContent = "";
         }
+      }
     });
-});
-
+  });
 
 //===============================
 /* SNACKBAR VALIDATIONS */
 
-document.getElementById("contactForm").addEventListener("submit", function(event){
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
     var snackbar = document.getElementById("snackbar");
     snackbar.className = "show";
-    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+    setTimeout(function () {
+      snackbar.className = snackbar.className.replace("show", "");
+    }, 3000);
   });
